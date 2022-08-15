ab79b9520c8e1b1cce7c4b48f2b6849f492e8544
TaeyeongHan <taeyeonghan@dable.io>
2022-08-15 15:58:10 +0900

    setProfileImage

--- src/lib/api/APIRoutes.ts
+++ src/lib/api/APIRoutes.ts
@@ -2,3 +2,4 @@ const host = 'http://localhost:5000'
 export const RegisterRoute=`${host}/auth/register`;
 export const LoginRoute=`${host}/auth/login`;
 export const FetchProfileImagesRoute=`${host}/profile/fetchProfileImages`
+export const SetProfileImageRoute =`${host}/profile/setProfileImage`
--- src/pages/SetProfilePage.tsx
+++ src/pages/SetProfilePage.tsx
@@ -5,13 +5,13 @@ import axios from 'axios';
 
 import toast, { Toaster } from 'react-hot-toast';
 import CubeLoader from '../components/CubeLoader';
-import { FetchProfileImagesRoute } from '../lib/api/APIRoutes'
-import {Buffer} from 'buffer'
+import { FetchProfileImagesRoute, SetProfileImageRoute } from '../lib/api/APIRoutes'
+import { Buffer } from 'buffer'
 
 function SetProfilePage() {
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(true);
-    const [selectedProfileImage, setSelectedProfileImage] = useState(0)
+    const [selectedProfileBuffer, setSelectedProfileBuffer] = useState(0)
     const [profileBuffers, setProfileBuffers] = useState<string[]>([]);
 
     useEffect(() => {
@@ -30,22 +30,45 @@ function SetProfilePage() {
         fetchProfileImages()
     }, [])
 
+    const setProfilePicture = async () => {
+        const user = JSON.parse(localStorage.getItem('chat-app-user') || "")
+        if (!user) {
+            toast.error('fail to get userData please logout and login again!')
+            return
+        }
+
+        const res = await axios.post(`${SetProfileImageRoute}/${user._id}`, {
+            image: profileBuffers[selectedProfileBuffer]
+        })
+
+        console.log(res);
+
+        const data = res.data;
+
+        user.profileImage = data.image
+        localStorage.setItem('chat-app-user', JSON.stringify(user))
+        navigate('/')
+
+        toast('Successfully set profile image')
+    }
+
     return (<>
         <div>SetProfilePage</div>
-        {isLoading ? <CubeLoader /> : <Container>
+        {isLoading ? <Container> <CubeLoader /></Container> : <Container>
             <div className="title-container">
                 <h1>Pick your profile image</h1>
             </div>
 
             <div className="profile-images">
-                {profileBuffers.map((profileBuffer, index) => <div key={'profile-image' + index} className={`profile-image ${selectedProfileImage === index && 'selected'}`}>
+                {profileBuffers.map((profileBuffer, index) => <div key={'profile-image' + index} className={`profile-image ${selectedProfileBuffer === index && 'selected'}`}>
                     <img
                         src={`data:image/svg+xml;base64,${profileBuffer}`}
-                        alt={"profile"+index}
-                        onClick={() => setSelectedProfileImage(index)}
+                        alt={"profile" + index}
+                        onClick={() => setSelectedProfileBuffer(index)}
                     />
                 </div>)}
             </div>
+            <button className={"submit-button"} onClick={() => setProfilePicture()}>Select</button>
         </Container>}
 
         <Toaster />
@@ -73,12 +96,34 @@ const Container = styled.div`
         gap:2rem;
         .profile-image{
             border:0.4rem solid transparent;
+            padding:0.4rem;
+            border-radius:5rem;
+            display:flex;
+            justify-content: center ;
+            align-items: center;
+            transition: 0.5s ease-in-out;
+
             img{
                 height:6rem;
             }
         }
+        .selected{
+            border:0.5rem solid #4e0
+        }
     }
-
+    .submit-button{
+      background-color:#997af0;
+      color:white ;
+      padding:1rem 2rem;
+      border:none;
+      cursor: pointer;
+      border-radius:0.4rem ;
+      font-size: 1rem;
+      transition:0.5 ease-in-out ;
+      &:hover{
+        background-color:#4e0eff;
+        }
+    }
 
 
 `
