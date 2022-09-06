159d0a6f3a5152f3e4abe8c3a0fbdeaddb7744bc
TaeyeongHan <taeyeonghan@dable.io>
2022-09-06 09:53:38 +0900

    fixed height of screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,78 @@
-import { lazy, Suspense } from 'react';
-import './App.css';
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
 
-import {  BrowserRouter,Link, Route, Routes } from 'react-router-dom';
-import CubeLoader from './components/CubeLoader';
+import "./App.css";
+
+import CubeLoader from "./components/CubeLoader";
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     exact: true,
-    element: lazy(() => import('./pages/MainPage')),
+    element: lazy(() => import("./pages/MainPage")),
   },
   {
-    path: '/login',
+    path: "/login",
     exact: true,
-    element: lazy(() => import('./pages/LoginPage')),
+    element: lazy(() => import("./pages/LoginPage")),
   },
   {
-    path: '/register',
+    path: "/register",
     exact: true,
-    element: lazy(() => import('./pages/RegisterPage')),
+    element: lazy(() => import("./pages/RegisterPage")),
   },
   {
-    path: '/setProfile',
+    path: "/setProfile",
     exact: true,
-    element: lazy(() => import('./pages/SetProfilePage')),
+    element: lazy(() => import("./pages/SetProfilePage")),
   },
   {
-    path:'/chat',
-    exact:true,
-    element:lazy(()=>import('./pages/ChatPage'))
-  }
+    path: "/chat",
+    exact: true,
+    element: lazy(() => import("./pages/ChatPage")),
+  },
 ];
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/register">Register</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/setProfile">SetProfile</Link></li>
-          <li><Link to="/chat">Chat</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        {routes.map((route, i) => {
-          return (
-            <Route
-              key={i.toString() + route.path}
-              path={route.path}
-              element={
-                <Suspense fallback={<CubeLoader/>}>
-                  <route.element />
-                </Suspense>
-              }
-            />
-          );
-        })}
-      </Routes>
-    </div>
+      <div className="App">
+        <NavContainer>
+          <Link to="/" className='nav-link'>Home</Link>
+          <Link to="/register" className='nav-link'>Register</Link>
+          <Link to="/login" className='nav-link'>Login</Link>
+          <Link to="/setProfile" className='nav-link'>SetProfile</Link>
+          <Link to="/chat" className='nav-link'>Chat</Link>
+        </NavContainer>
+        <Routes>
+          {routes.map((route, i) => {
+            return (
+              <Route
+                key={i.toString() + route.path}
+                path={route.path}
+                element={
+                  <Suspense fallback={<CubeLoader />}>
+                    <route.element />
+                  </Suspense>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
 
-export default App
+const NavContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content:space-around ;
+  .nav-link{
+    font-size:2rem;
+    margin-right:1rem;
+  }
+`;
+
+export default App;
