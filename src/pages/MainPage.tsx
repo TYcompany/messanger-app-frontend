0edcae568aa53031f5d9a252492507149cfff314
TaeyeongHan <taeyeonghan@dable.io>
2022-10-03 01:15:24 +0900

    sample store

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import IntlTelInput from "react-intl-tel-input";
+import "react-intl-tel-input/dist/main.css";
+
 function MainPage() {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const handlePhoneChange = (status: any, pn: any, country: any) => {
+    console.log(status, pn, country);
+    setPhoneNumber(pn)
+  };
   return (
     <div>
       <h1> Waicker</h1>
       What an interesting conversation!
+      <IntlTelInput
+        containerClassName="intl-tel-input"
+        inputClassName="form-control"
+        value={phoneNumber}
+        onPhoneNumberChange={handlePhoneChange}
+      />
     </div>
   );
 }
